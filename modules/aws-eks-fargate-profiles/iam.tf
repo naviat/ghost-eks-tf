110c0bce430e3895148bc3c407c60d1154f2e536
Hai Dam <vhai.ict@gmail.com>
2022-01-11 01:50:26 +0700

    Add fargate module

---
+++ modules/aws-eks-fargate-profiles/iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "fargate" {
+  name                  = "${var.eks_cluster_id}-${local.fargate_profiles["fargate_profile_name"]}"
+  assume_role_policy    = data.aws_iam_policy_document.fargate_assume_role_policy.json
+  force_detach_policies = true
+  tags                  = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate.name
+}
+
+resource "aws_iam_policy" "cwlogs" {
+  name        = "${var.eks_cluster_id}-${local.fargate_profiles["fargate_profile_name"]}-cwlogs"
+  description = "Allow fargate profiles to write logs to CloudWatch"
+  path        = var.path
+  policy      = data.aws_iam_policy_document.cwlogs.json
+}
+
+resource "aws_iam_role_policy_attachment" "cwlogs" {
+  policy_arn = aws_iam_policy.cwlogs.arn
+  role       = aws_iam_role.fargate.name
+}
---
+++ modules/aws-eks-fargate-profiles/variables.tf
@@ -0,0 +1,22 @@
+variable "fargate_profile" {
+  description = "Map of maps of `eks_node_groups` to create"
+  type        = any
+  default     = {}
+}
+
+variable "eks_cluster_id" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "path" {
+  type        = string
+  default     = "/"
+  description = "IAM resource path, e.g. /dev/"
+}
